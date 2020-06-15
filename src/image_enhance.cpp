686e689debe6fa15bec37a35dfbbae141fb9b26c
Frank Mascarich <fmascarich@gmail.com>
2020-06-15 11:05:09 -0700

    minor fixes

--- src/image_enhance.cpp
+++ src/image_enhance.cpp
@@ -65,9 +65,9 @@ void imageEnhance::callback_image_input(const sensor_msgs::ImageConstPtr &msg)
 				   CV_INTER_LINEAR);
 
 		Mat image_out(image_in.rows, image_in.cols, CV_8UC3);
-		Mat temp_image_out(image_in.rows, image_in.cols, CV_8UC3);
+		Mat old_image_out(image_in.rows, image_in.cols, CV_8UC3);
 		image_out = image_in;
-		temp_image_out = image_in;
+		old_image_out = image_in;
 
 		clock_t start = clock();
 
@@ -77,15 +77,15 @@ void imageEnhance::callback_image_input(const sensor_msgs::ImageConstPtr &msg)
 			image_in = cv::Scalar::all(255) - image_in;
 			unsigned char *indata = image_in.data;
 			unsigned char *outdata = image_out.data;
-			unsigned char *temp_outdata = temp_image_out.data;
+			unsigned char *old_outdata = old_image_out.data;
 			dehaze::CHazeRemoval hr;
 			hr.InitProc(image_in.cols, image_in.rows, image_in.channels());
-			hr.Process(indata, outdata, image_in.cols, image_in.rows, image_in.channels(), true);
-			hr.Process(indata, temp_outdata, image_in.cols, image_in.rows, image_in.channels(), false);
+			hr.Process(indata, outdata, image_in.cols, image_in.rows, image_in.channels(), false);
+			hr.Process(indata, old_outdata, image_in.cols, image_in.rows, image_in.channels(), true);
 			image_out = cv::Scalar::all(255) - image_out;
-			temp_image_out = cv::Scalar::all(255) - temp_image_out;
+			old_image_out = cv::Scalar::all(255) - old_image_out;
 			Mat diff_image(image_in.rows, image_in.cols, CV_8UC3);
-			cv::subtract(image_out, temp_image_out, diff_image);
+			cv::subtract(image_out, old_image_out, diff_image);
 			auto diff_sum = cv::sum(diff_image);
 			for(int i = 0; i < diff_sum.channels; i++)
 			{
